0d9393ca50daa3cedd37ce3eb5a9ea575c9b00d1
Daniel Esposito <danielesposito@macbkk.com>
2017-10-15 13:40:55 +0700

    Fixed spell var

--- minimosta.js
+++ minimosta.js
@@ -1,8 +1,5 @@
 var miniMonstaHealth = 10000;
 var miniMiniMonstaKill = 3000;
 var miniMiniMiniMonstaFear = 90000;
-<<<<<<< HEAD
 var miniProtectionSpell = 100;
-=======
-var miniRecoverySpell = 90;
->>>>>>> cfa8d8713d0385ed22ae3b0fab5aa7b4a0cc2818
+
